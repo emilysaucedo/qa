a74dd29740e756ede65d025b90954ea5c4d990e2
emilysaucedo <emilysaucedopires@gmail.com>
2025-11-07 17:59:21 -0300

    teste completo, com checkout

--- tests/saucedemo/comprar_mochila_strut.spec.js
+++ tests/saucedemo/comprar_mochila_strut.spec.js
@@ -19,6 +19,36 @@ async function loginClick(page, testInfo){
     await expect(page.locator('[data-test="title"]')).toHaveText('Products'); //testId
 }
 
+async function checkoutPage(page, testInfo){
+    await page.locator('[data-test="checkout"]').click();
+    await expect(page.locator('[data-test="title"]')).toHaveText('Checkout: Your Information'); //testId
+}
+
+async function checkoutFill(page, testInfo){
+    await page.locator('[data-test="firstName"]').fill('Emily');
+    await page.locator('[data-test="lastName"]').fill('Saucedo');
+    await page.locator('[data-test="postalCode"]').fill('06735182');
+    await page.locator('[data-test="continue"]').click();
+}
+
+async function checkoutFinish(page, testInfo){
+    await expect(page.locator('[data-test="title"]')).toHaveText('Checkout: Overview'); //testId
+    await expect(page.locator('[data-test="inventory-item-name"]')).toHaveText('Sauce Labs Backpack'); //testId
+    await expect(page.locator('[data-test="inventory-item-price"]')).toHaveText('$29.99'); //testId
+    await expect(page.locator('[data-test="payment-info-label"]')).toHaveText('Payment Information:'); //testId
+    await expect(page.locator('[data-test="total-label"]')).toHaveText('Total: $32.39'); //testId
+
+}
+
+async function checkoutComplete(page, testInfo){
+    await page.locator('[data-test="finish"]').click();
+    await expect(page.locator('[data-test="title"]')).toHaveText('Checkout: Complete!'); //testId
+    await expect(page).toHaveURL('/checkout-complete\.html'); 
+    await expect(page.locator('[data-test="complete-header"]')).toHaveText('Thank you for your order!'); //testId
+    await expect(page.locator('[data-test="complete-text"]')).toHaveText('Your order has been dispatched, and will arrive just as fast as the pony can get there!'); //testId
+    await expect(page.locator('[data-test="back-to-products"]')).toBeVisible(); //testId
+}
+
 
 test.describe('Saucedemo - Fluxo Principal de Compra', ()=>{
 
@@ -54,7 +84,26 @@ test.describe('Saucedemo - Fluxo Principal de Compra', ()=>{
 
             await snap(page, testInfo, 'TC001-Passo-5-Carrinho-Info-Verificado');
         })
-    
+
+        await test.step('Ir para a página de checkout', async ()=>{
+            await checkoutPage(page, testInfo);
+            await snap(page, testInfo, 'TC001-Passo-6-Checkout-Page');
+        })
+
+        await test.step('Preencher os dados de checkout', async ()=>{
+            await checkoutFill(page, testInfo);
+            await snap(page, testInfo, 'TC001-Passo-7-Checkout-Fill');
+        })
+
+        await test.step('Finalizar o checkout', async ()=>{
+            await checkoutFinish(page, testInfo);
+            await snap(page, testInfo, 'TC001-Passo-8-Checkout-Finish');
+        })
+
+        await test.step('Verificar o checkout completo', async ()=>{
+            await checkoutComplete(page, testInfo);
+            await snap(page, testInfo, 'TC001-Passo-9-Checkout-Complete');
+        })
     })
 
     test('Comprar mochila Detalhada', async ({ page }, testInfo) => {
@@ -93,6 +142,26 @@ test.describe('Saucedemo - Fluxo Principal de Compra', ()=>{
             await expect(page.locator('[data-test="inventory-item-name"]')).toHaveText('Sauce Labs Backpack'); //testId
             await expect(page.locator('[data-test="inventory-item-price"]')).toHaveText('$29.99'); //testId
             await snap(page, testInfo, 'TC001-Passo-6-Carrinho-Info-Verificado');
+        })
+
+        await test.step('Ir para a página de checkout', async ()=>{
+            await checkoutPage(page, testInfo);
+            await snap(page, testInfo, 'TC001-Passo-7-Checkout-Page');
+        })
+
+        await test.step('Preencher os dados de checkout', async ()=>{
+            await checkoutFill(page, testInfo);
+            await snap(page, testInfo, 'TC001-Passo-8-Checkout-Fill');
+        })
+
+        await test.step('Finalizar o checkout', async ()=>{
+            await checkoutFinish(page, testInfo);
+            await snap(page, testInfo, 'TC001-Passo-9-Checkout-Finish');
+        })
+
+        await test.step('Verificar o checkout completo', async ()=>{
+            await checkoutComplete(page, testInfo);
+            await snap(page, testInfo, 'TC001-Passo-10-Checkout-Complete');
         })
     })
 
